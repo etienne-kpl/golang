95c50836b84c7327d27b102c59042a2b424c5ca1
Etienne Klepal <etienne.klepal@gmail.com>
2023-04-16 19:19:19 +0200

    first loop

--- firstapp/main.go
+++ firstapp/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Golang lecture"
@@ -16,21 +19,35 @@ func main() {
 	var tickets uint
 	var bookings []string // Slice cause there is no size in the brackets
 
-	// ask the user for their name
-	// pointer & to store the variable
-	fmt.Println("Enter your first name:")
-	fmt.Scan(&firstName)
+	for {
+		// ask the user for their name
+		// pointer & to store the variable
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name:")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
 
-	fmt.Println("How many tickets do you want?")
-	fmt.Scan(&tickets)
+		fmt.Println("How many tickets do you want?")
+		fmt.Scan(&tickets)
 
-	remainingTickets -= tickets
-	bookings = append(bookings, "%v %v", firstName, lastName)
+		remainingTickets -= tickets
 
-	fmt.Printf("Thank you for your booking, %v %v!\n", firstName, lastName)
-	fmt.Printf("You booked %v tickets.\n", tickets)
-	fmt.Printf("Only %v tickets left for %v!\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you for your booking, %v %v!\n", firstName, lastName)
+		fmt.Printf("You booked %v tickets.\n", tickets)
+		fmt.Printf("Only %v tickets left for %v!\n", remainingTickets, conferenceName)
+
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("There are %v bookings:\n", len(bookings))
+
+		// range demande toujours 2 paramètres
+		// si on ne se sert pas de l'index, on doit mettre un _ à la place pour le dire au compileur
+		// for _, booking := range bookings {
+		// }
+		for index, booking := range bookings {
+			var names = strings.Fields(booking)
+			fmt.Printf("%v. %v %v.\n", index+1, names[0], names[1][0:1])
+		}
+	}
 }
